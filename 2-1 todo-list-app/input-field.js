c35b990ba517a78a0c2f7e7f2406885a717cddee
Javier Girón <115418134+Country4979@users.noreply.github.com>
2023-03-29 20:23:51 +0200

    Fin ejercio list Todo

--- 2-1 todo-list-app/input-field.js
+++ 2-1 todo-list-app/input-field.js
@@ -1,31 +1,33 @@
-/* input-field
+/*
+input-field
 
 atributos:
-    - buttonLabel
-    - placeholder
-    
-eventos:
-    - submit (value del input)
+- buttonLabel
+- placeholder
 
-    funcionamiento: Cuando el usuario hace click, recoge el valor del input y lo lanza
-    */
+eventos:
+- submit (value del input)
+
+funcionamiento: cuando el usuario pulse el botón de submit, debemos vaciar el input
+*/
 
 const templateElement = document.createElement("template");
 
 templateElement.innerHTML = `
 <style>
-.input-field-wrapper {
-    display: flex;
-}
 
-input {
-    flex-grow: 1
-}
+  .input-field-wrapper {
+    display: flex;
+  }
+
+  input {
+    flex-grow: 1;
+  }
 
 </style>
 
-<div class="keepcoding-component-wrapper">
-  <input type="text" placeholder="">
+<div class="input-field-wrapper">
+  <input type="text">
   <button></button>
 </div>
 
@@ -36,7 +38,7 @@ class InputField extends HTMLElement {
     super();
 
     this.attachShadow({ mode: "open" });
-    
+  
     //Lectura de los atributos que vamos atener
     this.buttonLabel = this.getAttribute('buttonLabel') || 'Añadir';
     this.placeholder = this.getAttribute('placeholder') || 'Escribe algo para añadirlo';
@@ -45,24 +47,25 @@ class InputField extends HTMLElement {
   connectedCallback() {
     const template = templateElement.content.cloneNode(true);
     this.shadowRoot.appendChild(template);
-    
-    const submitButton = this.shadowRoot.querySelector('button')
-    const inputElement = this.shadowRoot.querySelector('input')
 
-    submitButton.textContent = this.buttonLabel; //Pintamos el botón
-    inputElement.placeholder = this.placeholder;  //Pintamos el placholder del input
-    
-    submitButton.addEventListener('click', () => {
-        const inputValue = inputElement.value;  //Recopilamos el dato
+    const submitButtonElement = this.shadowRoot.querySelector('button');
+    const inputElement = this.shadowRoot.querySelector('input');
 
-        const event = new CustomEvent('submit', {
-            detail: inputValue
-        })
-        this.dispatchEvent(event); //Disparamos el evento
+    submitButtonElement.textContent = this.buttonLabel;   //Pintamos el botón
+    inputElement.placeholder = this.placeholder;          //Pintamos el placholder del input
 
-        inputElement.value = "";
-        })
-    }
+    submitButtonElement.addEventListener('click', () => {
+      const inputValue = inputElement.value;              //Recopilamos el dato
+
+      const event = new CustomEvent("submit", {
+        detail: inputValue
+      })
+
+      this.dispatchEvent(event);                          //Disparamos el evento
+
+      inputElement.value = "";
+    })
+  }
 }
 
 window.customElements.define("input-field", InputField);
